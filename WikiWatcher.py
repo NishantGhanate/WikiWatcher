edf63c440494d247abc5776979a23a1c8039c437
NishantGhanate <nishant7.ng@gmail.com>
2021-02-18 12:52:22 +0530

    5 minute data collection added

--- WikiWatcher.py
+++ WikiWatcher.py
@@ -1,22 +1,33 @@
+import os
+import sys
 import json
 import time
 import asyncio
 import aiohttp
+import traceback
+
+from datetime import datetime, timedelta
+from collections import deque
 from aiosseclient import aiosseclient
 
-from datetime import datetime
-import os
-
 SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
-
 WIKI_REPORT_PATH =  os.path.join(SCRIPT_DIR,"WikiReports")
+
+PREV_TIME = datetime.now()
+CURRENT_TIME = datetime.now()
 
 if not os.path.exists(WIKI_REPORT_PATH):
     os.makedirs(WIKI_REPORT_PATH)
 
-def filter_report(json_data):
-    maps = {}
+TIME_WINDOW = deque()
+MIN_5_REPORT = {} 
+COUNTER = 0 
 
+def filter_report(json_data):
+    """
+    Please readme.md for filtered data structure
+    """
+    maps = {}
     for data in json_data:
         performer = data['performer']
         user_edit_count = 0
@@ -68,6 +79,69 @@ def sort_report(json_data,page_rsort=True,user_rsort=True):
         json_data[k]['users'] = dict( sorted(users_list.items(), key= lambda x: x[1], reverse=user_rsort) )
     return keys
 
+def crunch_report():
+    """
+    For sliding window , here compressing the list of data
+    collected over 5 mintue to one single dict
+    """
+    global TIME_WINDOW
+    global MIN_5_REPORT
+    try :
+        for window in TIME_WINDOW:
+            for domain in window:
+                if domain not in MIN_5_REPORT:
+                    MIN_5_REPORT[domain] = window[domain]
+                else:
+                    # If domain & user exists update their values
+                    # or add new users in that domain
+                    MIN_5_REPORT[domain]['page_edit_count'] += window[domain]['page_edit_count']
+                    users_list = window[domain]['users']
+                    for user in users_list:
+                        if user in MIN_5_REPORT[domain]['users']:
+                            MIN_5_REPORT[domain]['users'][user] +=  users_list[user]
+                        else:
+                            MIN_5_REPORT[domain]['users'][user] = users_list[user]
+    except Exception as e :
+        print("\n Exception occured in crunch report {}".format(e))
+        print(traceback.print_exc)
+        sys.exit()
+
+def re_calucate(json_remove,json_add):
+    global MIN_5_REPORT
+
+    try :
+        for key in json_remove:
+            if key in MIN_5_REPORT:
+                MIN_5_REPORT[key]['page_edit_count'] -= json_remove[key]['page_edit_count']
+
+                users_list = json_remove[key]['users']
+                for user in users_list:
+                    if user in MIN_5_REPORT[key]['users']:
+                        MIN_5_REPORT[key]['users'][user] -= users_list[user]    
+    except Exception as e :
+        print("Exception occured in re_calculate removal {}".format(e))
+        
+    
+    try :
+        for key in json_add:
+            if key in MIN_5_REPORT:
+                MIN_5_REPORT[key]['page_edit_count'] += json_add[key]['page_edit_count']
+                users_list = json_add[key]['users']
+                for user in users_list:
+                    if user in MIN_5_REPORT[key]['users']:
+                        MIN_5_REPORT[key]['users'][user] += users_list[user]
+                    else:
+                        MIN_5_REPORT[key]['users'][user] = users_list[user]
+            
+            else:
+                MIN_5_REPORT[key] = json_add[key]
+
+        MIN_5_REPORT = sort_report(MIN_5_REPORT)
+        return MIN_5_REPORT
+
+    except Exception as e :
+        print("Exception occured in re_calculate during addition {}".format(e))
+        
 async def save_report(json_data):
     try:
         time_stamp =  datetime.now().strftime("%A- %d- %B %Y %I-%M-%S")
@@ -77,36 +151,106 @@ async def save_report(json_data):
     except Exception as e:
         print(e)
 
-async def print_console(json_data):
+async def print_console(json_data,prev_time,current_time):
+    print("\n{d1} Report generated from {prev_time} - {current_time} {d2}"
+    .format(
+            d1 = "#"*20,
+            prev_time = str(prev_time),
+            current_time = str(current_time),
+            d2 = "#"*20
+        )
+    )
+
     for data in json_data:
-        print("\nDomain : {} , {} pages updated "
+        print("\n{} : {} pages updated "
         .format(
                 data , 
                 json_data[data]['page_edit_count']
             )
         )
-        print("Users who made changes to {} ".format(data))
+        print("users:")
         user_list = json_data[data]['users']
         for user in user_list:
-            print("\t - {user} : {edit_count}"
+            print("\t- {user} : {edit_count}"
             .format(
                     user = user,
                     edit_count = user_list[user]
                 )
             )
 
-async def generate_report_1(json_data):
-    filtered_report = filter_report(json_data)
-    sorted_report  = sort_report(filtered_report)
-    #TODO: save report async based on event url & timestamp
-    await save_report(sorted_report)
-    await print_console(sorted_report)
+async def generate_report(json_data):
+    """ 
+    The global TIME_WINDOW will store past 5 min data 
+    for sliding window report i.e 1-6 , 2-7 ranges so on
 
+    The global COUNTER is only incremented first 5 times then
+    print SWR of every 5 mintues.
+
+    TODO:
+        - Use deque DS to keep window sliding 
+        - After 5 min, crunch the whole data into single report
+        - Then Pop left & append right in deque 
+        - Recalc Report : Subtract the popped values from single report 
+          and add/update append values   
+        - Lastly print they see me rolling 5 min sliding report  
+
+    """
+
+    global TIME_WINDOW
+    global COUNTER 
+    global PREV_TIME
+    global CURRENT_TIME 
+
+    filtered_report = filter_report(json_data)
+    sorted_report = sort_report(filtered_report)
+    
+    #TODO: save report async based on event url & timestamp
+    if COUNTER > 5:
+        # print("\nShowing report over last 5 minutes")
+        five_minute = timedelta(minutes=5)
+        PREV_TIME = CURRENT_TIME - five_minute
+        json_remove = TIME_WINDOW.popleft()
+        result = re_calucate(
+            json_remove = json_remove,
+            json_add = sorted_report
+        )
+
+        await print_console(
+            json_data= result,
+            prev_time=  PREV_TIME,
+            current_time= CURRENT_TIME
+        ) 
+        TIME_WINDOW.append(sorted_report)
+    else: 
+        COUNTER += 1
+        one_minute = timedelta(minutes=1)
+        PREV_TIME = CURRENT_TIME - one_minute
+        await print_console(
+            json_data = sorted_report,
+            prev_time=  PREV_TIME,
+            current_time= CURRENT_TIME
+        )
+        TIME_WINDOW.append(sorted_report)
+
+        if COUNTER == 5:
+            crunch_report()
+    # await save_report(sorted_report)
+            
 async def read_stream(session):
+    """
+    Store the stream data for 1 min in temp on message event
+    Every 1 min call generate report 
+    """
+    global CURRENT_TIME 
+
     temp = []
-    start_1 = start_5 = time.time()
+    start = time.time()
+    url = 'https://stream.wikimedia.org/v2/stream/revision-create'
     try:
-        async for event in aiosseclient('https://stream.wikimedia.org/v2/stream/revision-create'):
+        print("\nFetching url..... ")
+        print("\nPlease wait for 1 mniute while we collect data & generate report")
+
+        async for event in aiosseclient(url):
             if event.event == 'message':
                 try:
                     change = json.loads(event.data)    
@@ -114,20 +258,19 @@ async def read_stream(session):
                     pass
                 temp.append(change)
                 done = time.time()
-                elapsed = done - start_1
-                if elapsed >= 5:
-                    start_1 = done
-                    await generate_report_1(json_data= temp)
+                elapsed = done - start
 
-                # elapsed = done - start_5
-                # if elapsed >= 300:
-                #     start_5 = time.time()
-                    
+                if elapsed >= 60:
+                    start = done
+                    CURRENT_TIME = datetime.now()
+                    await generate_report(json_data= temp)
+            
     except Exception as e:
         print(e)
         return await read_stream(session)
 
 async def main():
+    print("\nInitializing session ..")
     async with aiohttp.ClientSession() as session:
         never = await read_stream(session)
 
@@ -135,8 +278,9 @@ async def main():
 loop = asyncio.get_event_loop()
 try:
    loop.run_until_complete(main())
-except KeyboardInterrupt:
+except :
     pass
 finally:
-    print("Closing Program")
-    loop.close()
+    print("\nProgram closed")
+    exit()
+    
